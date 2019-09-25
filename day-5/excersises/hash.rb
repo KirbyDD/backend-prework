ee0489a2778cc5100f512480e312f82973ad6cce
DeMarcus Kirby <tkd024@gmail.com>
2019-09-25 12:15:53 -0700

    Add hash lesson

---
+++ day-5/excersises/hash.rb
@@ -0,0 +1,27 @@
+# In the exercises below, write your own code where indicated
+# to achieve the desired result. You should be able to run this
+# file from your terminal with the command `ruby day_5/exercises/hashes.rb`
+
+# example: Write code below to print a hash that holds grocery store inventory
+foods = {apples: 23, grapes: 507, eggs: 48}
+p foods
+
+# Write code below that will print a hash of animals and their number
+# at the zoo. (an inventory of animals)
+zoo = {lions: 40, tigers: 45, bears: 50}
+p zoo
+
+# Using the zoo that you created above, print all the keys in the hash.
+p zoo.keys
+
+# Using the zoo that you created above, print all the values in the hash.
+p zoo.values
+
+# Using the zoo that you created above, print the value of the first item in
+# the hash
+first = zoo.keys[0]
+p zoo[first]
+
+# Add an animal to the zoo hash and print the updated hash.
+zoo['elephants'] = 55
+p zoo
